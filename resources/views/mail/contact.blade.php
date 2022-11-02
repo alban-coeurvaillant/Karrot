dc0cc5c8c49253c110f1e254d40f1fafaaed367e
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2022-11-02 21:48:57 +0100

    layering contact component

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,7 +1,10 @@
 @component('mail::message')
 # {{ __('New contact') }}
 
+<p>
 {{ __('Firstname') }}: {{ $contact->firstname }}  
+</p>
+
 {{ __('Lastname') }}: {{ $contact->lastname }}  
 {{ __('Email') }}: {{ $contact->email }}  
 
