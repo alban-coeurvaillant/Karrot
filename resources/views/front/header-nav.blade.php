75a092c17b85cb40ff54f313f0aae0934b4d9f64
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2022-11-12 15:58:19 +0100

    Template structure cleaning

--- resources/views/front/header-nav.blade.php
+++ resources/views/front/header-nav.blade.php
@@ -1,42 +1,45 @@
-<header class="o-header">
+<header class="o-master-header">
     <a class="navbar-brand" href="#">
         <picture class="">
             <img src="{{ asset('img/GD-logo.svg') }}" loading="lazy">
         </picture>
     </a>
-    <span  class="navbar-brand-baseline">
-    Chants authentiques de l'église Afro-Américaine
-    </span>
+    <div class="navbar-brand-baseline">
+    <p>Chants authentiques de l'église Afro-Américaine</p>
+    </div>
 </header>
 
-<ul class="nav nav-tabs flex-column nav-fill d-flex justify-content-center align-items-start flex-sm-column align-items-sm-center flex-xl-row flex-xl-nowrap flex-xxl-row flex-xxl-nowrap" style="background: #4a0740;border-style: none;">
-    
-    <li class="nav-item flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch">
-        <a class="nav-link  @if (url()->current() == url('/')) active @endif fw-semibold flex-grow-1" href="/" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">
+<ul class="o-main-nav">
+    <li class="nav-item">
+        <a class="@if (url()->current() == url('/')) active @endif fw-semibold flex-grow-1" href="/">
         Notre chorale
         </a>
     </li>
 
-    <li class="nav-item flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch" style="border-style: none;">
-        <a class="nav-link @if (url()->current() == url('histoire-du-gospel')) active @endif fw-semibold flex-grow-1" href="{{ url('histoire-du-gospel') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">L'histoire du gospel</a>
+    <li class="nav-item">
+        <a class="@if (url()->current() == url('histoire-du-gospel')) active @endif" href="{{ url('histoire-du-gospel') }}">L'histoire du gospel</a>
     </li>
+
     @if (config('karrot.event'))
-        <li class="nav-item flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch">
-            <a class="nav-link @if (request()->routeIs('event.index')) active @endif fw-semibold flex-grow-1" href="{{ route('event.index') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">Concerts et reservations</a>
+    <li class="nav-item">
+            <a class="@if (request()->routeIs('event.index')) active @endif " href="{{ route('event.index') }}" >Concerts et reservations</a>
         </li>
     @endif
+
     @if (config('karrot.gallery'))
-    <li class="nav-item flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch">
-        <a class="nav-link @if (request()->routeIs('gallery.index')) active @endif fw-semibold flex-grow-1" href="{{ route('gallery.index') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">Galerie photo</a>
+    <li class="nav-item">
+        <a class=" @if (request()->routeIs('gallery.index')) active @endif" href="{{ route('gallery.index') }}">Galerie photo</a>
     </li>
     @endif
-    <li class="nav-item @if (url()->current() == url('presse')) active @endif flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch">
-        <a class="nav-link fw-semibold flex-grow-1" href="{{ url('presse') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">Presse</a>
+
+    <li class="nav-item @if (url()->current() == url('presse')) active @endif">
+        <a href="{{ url('presse') }}">Presse</a>
     </li>
-    <li class="nav-item @if (url()->current() == url('discographie')) active @endif flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch">
-        <a class="nav-link fw-semibold flex-grow-1" href="{{ url('discographie') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">Discographie</a>
+    <li class=" nav-item @if (url()->current() == url('discographie')) active @endif">
+        <a href="{{ url('discographie') }}">Discographie</a>
     </li>
     @if (config('karrot.contact'))
-        <li class="nav-item flex-grow-0 align-self-stretch align-self-sm-stretch align-self-md-stretch align-self-lg-stretch"><a class="nav-link @if (request()->routeIs('contact.index')) active @endif fw-semibold flex-grow-1" href="{{ route('contact.index') }}" style="border-radius: 0;color: var(--bs-nav-tabs-link-active-bg);">Contactez-nous</a></li>
+    <li class="nav-item">
+            <a class="@if (request()->routeIs('contact.index')) active @endif" href="{{ route('contact.index') }}">Contactez-nous</a></li>
     @endif
 </ul>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -12,29 +12,21 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=EB+Garamond&amp;display=swap">
     <link rel="stylesheet" href="{{ asset('fonts/fontawesome-all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/gs-colors.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header-Footer-Clean.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header-Navigation-Clean1.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
-    <link rel="stylesheet" href="{{ asset('css/Navbar-Right-Links-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Pretty-Footer-.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     {{ $scripts ?? null }}
 </head>
-<body class="bg-dark">
+<body>
 <div class="main-wrapper container-fluid mx-auto">
     @include('front.header-nav')
-
-    <div class="p-3 section--tiltle">
+    <div class="o-section-title">
         <h1>{{ $h1 }}</h1>
     </div>
-    <div id="main-1" class="bg-light">
-        <div>
-            <div class="row d-flex flex-row flex-sm-column flex-md-column flex-lg-column flex-xl-row flex-xxl-row container-fluid" style="margin: 0;padding: 2em;background: #efefef;">
-                <div class="col-12 col-xl-9 col-xxl-9 col-9">
+    <div id="main" class="o-main-content">
+                <div class="main-column">
                     {{ $slot }}
                 </div>
-                <div class=" col-10 col-sm-10 col-xl-3 col-xxl-3 col-3 aside-column">
+                <div class="aside-column">
                     <h3>Réservations</h3>
                     <picture>
                         <img src="{{ asset('img/affiche.jpg') }}">
@@ -47,16 +39,27 @@
                     </button>
                     <h3>Presse Pack</h3>
                 </div>
-            </div>
-        </div>
     </div>
     
-    <footer class="d-flex flex-column justify-content-between align-items-center flex-xl-row flex-xxl-row p-2" style="background: #4a0740;margin: 0;border-bottom-right-radius: 2em;border-bottom-left-radius: 2em;min-height: 3em;">
-        <div class="d-flex flex-column align-items-center flex-sm-column flex-md-column flex-lg-column flex-xl-row flex-xxl-row">
-            <p class="fw-normal p-2" style="margin: 0;font-size: 0.9em;color: var(--bs-gray-400);">&nbsp;© 2022 - Tous droits réservés www.gospeldream.fr&nbsp;</p><a class="fw-lighter" href="#" style="font-size: 0.9em;color: #b3a38e;">Consultez nos mentions légales</a>
+    <footer>
+        <div class="footer-mentions">
+            <p>&nbsp;© 2022 - Tous droits réservés www.gospeldream.fr&nbsp;</p>
+            <a href="#">Consultez nos mentions légales</a>
         </div>
-        <div class="d-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="margin-right: 1em;">
-            <p class="fw-light d-xxl-flex align-items-xxl-center p-2" style="margin: 0;font-size: 0.9em;color: var(--bs-gray-400);">Retrouvez nous sur nos sur :</p><a href="#"><i class="fab fa-facebook m-2" style="color: #b3a38e;"></i></a><a href="#"><i class="far fa-star m-1" style="color: #b3a38e;"></i></a><a href="#"><i class="fab fa-instagram m-1" style="color: #b3a38e;"></i></a>
+        <div class="footer-socials">
+            <p>Retrouvez nous sur nos sur :</p>
+            <a href="#">
+                <i class="fab fa-facebook m-2">
+                </i>
+            </a>
+            <a href="#">
+                <i class="far fa-star m-1">
+                </i>
+            </a>
+            <a href="#">
+                <i class="fab fa-instagram m-1">
+                </i>
+            </a>
         </div>
     </footer>
 </div>
