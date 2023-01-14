1689403615f7d5e991c9fea363136f45e4cb8192
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-01-14 15:28:52 +0100

    Add new K components 👏🏾

--- resources/views/front/disc/index.blade.php
+++ resources/views/front/disc/index.blade.php
@@ -3,12 +3,14 @@
         Discographie
     @endslot
 
-    <section class="py-4 py-xl-5">
-        <div class="container">
+    <section class="k-section">
+        <div class="k-container">
             @foreach($discs as $disc)
-            <div class="row mb-5">
-                <div class="col-auto"><img class="img-fluid" src="{{ asset($disc->image_path) }}" alt=""></div>
-                <div class="col-12 col-sm-8 col-md-7 col-xl-8">
+            <div class="k-row">
+                <div class="">
+                <img class="img-fluid" src="{{ asset($disc->image_path) }}" alt="">
+                </div>
+                <div class="">
                     <div>{{ $disc->title }}</div>
                     <div><strong>{{ $disc->subtitle }}</strong></div>
                     <div>{{ $disc->description }}</div>
