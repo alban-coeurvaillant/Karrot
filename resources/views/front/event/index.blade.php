d6c656858e8c08fee813b24f6c669eaa5855def6
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-01-22 07:50:55 +0100

    update event layout

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -13,21 +13,19 @@
             <h4>{{ $event->place }}</span><br></h4>
             </div>
             <div class="card-description">
-            <div class="event-description">
                 <div class="event-image">img</div>
                     <p>{{ $event->description }}</p>
-                      <div class="event-time">
+                  <div class="event-time">
                         <span class="date">
                             {{ ucfirst($event->date->isoFormat('dddd')) }}   
                             <i class="far fa-star"></i>
                             {{ $event->date->format('d/m/y') }}      
                         </span>
                         <span class="time">{{ $event->time }}</span>
-                        </div> 
                     </div> 
-            <div class="event-reservation">
+                </div> 
+             <div class="event-reservation">
                 <x-button-link href="{{ route('event.reservation', $event) }}">{{ __('Book') }}</x-button-link>
-            </div>
             </div>
         </div>
         @endforeach
