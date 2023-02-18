bd65ee90b92413009d5adcef4d672204ec380334
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 21:15:40 +0100

    new main-container layout

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,6 +1,4 @@
 @component('layouts.front')
-    
-    
     @slot('h1')
         Gospel Dream
     @endslot
@@ -11,19 +9,13 @@
     </p>
     <p>Les uns sont d’origine africaine, les autres viennent des Antilles et des Etats-Unis.</p>
     <p>La chorale Gospel dream est un ensemble mixte et cosmopolite de chanteurs et musiciens noirs qui réunit :</p>
-
-    <ul class="list style">
-        <li>10 à 12 choristes</li>
-        <li>1 pianiste</li>
-        <li>1 trompettiste.</li>
-    </ul>
-
+    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
     <p>Sur scène, la chorale respire la joie de vivre, avec des voix qui parcourent toutes les nuances, des murmures aux
         vocalises improvisées, leur concert est conçu pour permettre au public de participer, de témoigner et partager
         un hymne à l’amour.</p>
-
-
-
-    
-
+        <div class="dashboard">
+            <span>1</span>
+            <span>1</span>
+            <span>1</span>
+        </div>
 @endcomponent
