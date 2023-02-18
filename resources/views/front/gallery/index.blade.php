110478e0f1d855cac76b13c8a9696a0ed9ba04f1
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 16:25:48 +0100

    new o-vents layout

--- resources/views/front/gallery/index.blade.php
+++ resources/views/front/gallery/index.blade.php
@@ -5,6 +5,8 @@
 
     <section class="o-photo-gallery ">
         <div class="container">
+        <h3>Découvrez la chorale</h3>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             <div class="photos" data-bss-baguettebox="">
                 @foreach($images as $image)
                 <div class="photo">
