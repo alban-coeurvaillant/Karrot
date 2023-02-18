0fb1277a7255d361f13d1995c920bb0c75cbb3da
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 10:38:16 +0100

    new o-vents layout

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -11,8 +11,8 @@
         @foreach($row as $event)
         <div class="o-card-event">
                 <span class="event-place d-flex ai--c p-3">{{ $event->place }}</span>
-                <span class="event-image d-flex ai--c p-3">img</span>
-                <span class="event-description d-flex ai--c p-3">{{ $event->description }}</span>
+                <span class="event-image d-flex ai--c p-3 d-none">img</span>
+                <span class="event-description d-flex ai--c p-3 d-none">{{ $event->description }}</span>
                 <span class="day d-flex ai--c p-3"> {{ ucfirst($event->date->isoFormat('dddd')) }} </span>
                 <span class="date d-flex ai--c p-3">{{ $event->date->format('d/m/y') }}  </span>
                 <span class="time d-flex ai--c p-3">{{ $event->time }}</span>
