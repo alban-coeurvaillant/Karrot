79ce8f9356d106627e24ab645131a67c95bb2677
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 22:34:11 +0100

    adjust main layout

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -12,11 +12,11 @@
         <div class="align-items-center d-flex flex-column mt-0 o-card-event p-3">
                 <span class="ai--c d-flex event-place h5">{{ $event->place }}</span>
                 <div class="event__content">
-                <span class="event-image d-flex ai--c d-none">img</span>
-                <span class="event-description d-flex ai--c d-none">{{ $event->description }}</span>
-                <span class="day d-flex ai--c"> {{ ucfirst($event->date->isoFormat('dddd')) }} </span>
-                <span class="date d-flex ai--c">{{ $event->date->format('d/m/y') }}  </span>
-                <span class="time d-flex ai--c">{{ $event->time }}</span>
+                <span class="event-image d-flex ai--c d-none mx-2">img</span>
+                <span class="event-description d-flex ai--c d-none mx-2">{{ $event->description }}</span>
+                <span class="day d-flex  mx-2"> {{ ucfirst($event->date->isoFormat('dddd')) }} </span>
+                <span class="date d-flex mx-2">{{ $event->date->format('d/m/y') }}  </span>
+                <span class="time d-flex mx-2">{{ $event->time }}</span>
                 </div>
                 <span class="event-reservation w-100">
                     <x-button-link  href="{{ route('event.reservation', $event) }}" class="btn btn-primary mt-2">{{ __('Book') }}</x-button-link>
