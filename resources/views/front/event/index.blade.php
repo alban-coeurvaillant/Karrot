f01f8429e828c73b97841a2b86058e50d0ae4654
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 22:27:00 +0100

    adjust main layout

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -11,12 +11,14 @@
         @foreach($row as $event)
         <div class="align-items-center d-flex flex-column mt-0 o-card-event p-3">
                 <span class="ai--c d-flex event-place h5">{{ $event->place }}</span>
+                <div class="event__content">
                 <span class="event-image d-flex ai--c d-none">img</span>
                 <span class="event-description d-flex ai--c d-none">{{ $event->description }}</span>
                 <span class="day d-flex ai--c"> {{ ucfirst($event->date->isoFormat('dddd')) }} </span>
                 <span class="date d-flex ai--c">{{ $event->date->format('d/m/y') }}  </span>
                 <span class="time d-flex ai--c">{{ $event->time }}</span>
-               <span class="event-reservation w-100">
+                </div>
+                <span class="event-reservation w-100">
                     <x-button-link :primary="false" href="{{ route('event.reservation', $event) }}">{{ __('Book') }}</x-button-link>
                 </span>
         </div>
