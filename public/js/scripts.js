11af35eb9eb81bddaf36a2571f3c0e759f06f884
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-18 09:36:08 +0100

    Updates

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,19 @@
-(()=>{var e;e=document.getElementById("main-nav"),document.getElementById("mobile-btn").addEventListener("click",(function(){e.classList.toggle("is-display")}))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/gospel.js ***!
+  \********************************/
+var mainMenu;
+mainMenu = document.getElementById("main-nav");
+document.getElementById("mobile-btn").addEventListener("click", mobileBehaviour);
+
+function mobileBehaviour() {
+  //alert("Ho")
+  displayMenu();
+}
+
+function displayMenu() {
+  mainMenu.classList.toggle("is-display");
+}
+/******/ })()
+;
