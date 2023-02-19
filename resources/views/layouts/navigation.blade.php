ac6765c866e5cbbf81a977cfbed48de9364f9171
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-19 14:23:48 +0100

    update css

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,43 +10,43 @@
 
         <div id="navbarSupportedContent">
             <ul class="navbar-nav">
-                <li class="nav-item">
+                <li class="nflex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </li>
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.content.index')" :active="request()->routeIs('admin.content.*')">
                         {{ __('Contents') }}
                     </x-nav-link>
                 </li>
                 @if (config('karrot.event'))
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.event.index')" :active="request()->routeIs('admin.event.*')">
                         {{ __('Events') }}
                     </x-nav-link>
                 </li>
                 @endif
                 @if (config('karrot.gallery'))
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.gallery.index')" :active="request()->routeIs('admin.gallery.*')">
                         {{ __('Gallery') }}
                     </x-nav-link>
                 </li>
                 @endif
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.disc.index')" :active="request()->routeIs('admin.disc.*')">
                         {{ __('Discography') }}
                     </x-nav-link>
                 </li>
                 @if (config('karrot.contact'))
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('admin.contact.index')" :active="request()->routeIs('admin.contact.*')">
                         {{ __('Contacts') }}
                     </x-nav-link>
                 </li>
                 @endif
-                <li class="nav-item">
+                <li class="flex-grow-1 nav-item p-2 text-center">
                     <x-nav-link :href="route('logout')">
                         {{ __('Se déconnecter') }}
                     </x-nav-link>
