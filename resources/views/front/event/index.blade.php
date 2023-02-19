625b22055c040b7b34b2eee584e14281f518db96
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-19 15:07:53 +0100

    update header-nav classes

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -9,7 +9,7 @@
         </div>
          <div class="o-events mt-3">
         @foreach($row as $event)
-        <div class="align-items-center d-flex flex-column mt-0 o-card-event p-3">
+        <div class="align-items-center d-flex flex-column mt-0 o-card-event p-3 w-50">
                 <span class="ai--c d-flex event-place h5">{{ $event->place }}</span>
                 <div class="event__content d-flex">
                 <span class="event-image d-flex ai--c d-none mx-2">img</span>
@@ -18,7 +18,7 @@
                 <span class="date d-flex mx-2">{{ $event->date->format('d/m/y') }}  </span>
                 <span class="time d-flex mx-2">{{ $event->time }}</span>
                 </div>
-                <span class="event-reservation w-100">
+                <span class="event-reservation">
                     <x-button-link  href="{{ route('event.reservation', $event) }}" class="btn btn-primary mt-2">{{ __('Book') }}</x-button-link>
                 </span>
         </div>
