853684fdcd6c6119ae732c6661c2c99bdd54ad82
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-23 19:28:03 +0100

    update event layout

--- resources/views/front/event/index.blade.php
+++ resources/views/front/event/index.blade.php
@@ -9,17 +9,17 @@
         </div>
          <div class="o-events container mt-3">
         @foreach($row as $event)
-        <div class="align-items-center col d-flex flex-column flex-lg-row mt-0 o-card-event mx-3">
-                <span class="d-flex event-place fs-6">{{ $event->place }}</span>
-                <div class="d-flex event__content flex-grow-1 fs-6">
+        <div class="align-items-center border col d-flex flex-column flex-lg-row mt-0 o-card-event p-2">
+                <span class="bg-danger d-flex w-50">{{ $event->place }}</span>
+                <div class="bg-danger d-flex m-2 order-firsts">
                 <span class="event-image d-flex d-none mx-2">img</span>
                 <span class="event-description d-flex d-none mx-2">{{ $event->description }}</span>
                 <span class="day d-flex  mx-2"> {{ ucfirst($event->date->isoFormat('dddd')) }} </span>
                 <span class="date d-flex mx-2">{{ $event->date->format('d/m/y') }}  </span>
                 <span class="time d-flex mx-2">{{ $event->time }}</span>
                 </div>
-                <span class="align-items-center d-flex event-reservation flex-grow-1 justify-content-end w-auto">
-                    <x-button-link  href="{{ route('event.reservation', $event) }}" class="btn btn-primary mt-2 h-auto">{{ __('Book') }}</x-button-link>
+                <span class="d-flex flex-grow-1 justify-content-centero">
+                    <x-button-link  href="{{ route('event.reservation', $event) }}" class="btn btn-primary h-auto border-0 w-100">{{ __('Book') }}</x-button-link>
                 </span>
         </div>
         @endforeach
