1d92289ab845824e01649bb7898cc2478e91bda7
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-23 21:05:47 +0100

    update event layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,23 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-     
         
     </head>
     <body class="o-admin-page">
         <div class="main-wrapper">
             @include('layouts.navigation')
-        
-        
             <header class="o-admin-header">
                     {{ $header }}
             </header>
-               
             
             <main class="main-container">
                 {{ $slot }}
