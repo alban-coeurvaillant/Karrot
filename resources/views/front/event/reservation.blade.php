8f3f9b3763b14cde7860ce683e36a57d125e5621
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-02-23 20:21:10 +0100

    update event layout

--- resources/views/front/event/reservation.blade.php
+++ resources/views/front/event/reservation.blade.php
@@ -33,25 +33,25 @@
         @csrf
 
         <div class="mb-3 row w-100">
-            <label for="lastname" class="col-sm-4 col-md-3 col-form-label">{{ __('Lastname') }} *</label>
-            <div class="col-sm-8 col-md-6">
+            <label for="lastname" class="col-form-label">{{ __('Lastname') }} *</label>
+            <div class="">
                 <input type="text" name="lastname" id="lastname" class="form-control" value="{{ old('lastname') }}">
             </div>
         </div>
         <div class="mb-3 row w-100">
-            <label for="firstname" class="col-sm-4 col-md-3 col-form-label">{{ __('Firstname') }} *</label>
-            <div class="col-sm-8 col-md-6">
+            <label for="firstname" class="col-form-label">{{ __('Firstname') }} *</label>
+            <div class="">
                 <input type="text" name="firstname" id="firstname" class="form-control" value="{{ old('firstname') }}">
             </div>
         </div>
         <div class="mb-3 row w-100">
-            <label for="email" class="col-sm-4 col-md-3 col-form-label">{{ __('Email') }} *</label>
-            <div class="col-sm-8 col-md-6">
+            <label for="email" class="col-form-label">{{ __('Email') }} *</label>
+            <div class="">
                 <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
             </div>
         </div>
         <div class="mb-3 row w-100">
-            <label for="nb_seats" class="col-sm-5 col-md-3 col-form-label">{{ __('Nb seats') }} *</label>
+            <label for="nb_seats" class="col-form-label">{{ __('Nb seats') }} *</label>
             <div class="col-auto">
                 <select name="nb_seats" id="nb_seats" class="form-control">
                     @foreach(range(1, min(50, $event->seats)) as $nb)
@@ -66,7 +66,7 @@
         </div>
 
         <div
-            class="h-captcha mb-3"
+            class="h-captcha w-100"
             data-sitekey="{{ env('HCAPTCHA_SITEKEY') }}"
         ></div>
 
