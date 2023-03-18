4c369cdb90460ab93a8f0bcabf886123ca851b7a
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-03-18 09:15:32 +0100

    changement couleur header

--- resources/views/front/header-nav.blade.php
+++ resources/views/front/header-nav.blade.php
@@ -1,20 +1,17 @@
-<header class="d-flex flex-column o-master-headerp-4 p-4 bg-black">
-<div class="col-form-label-lg d-flex flex-column flex-lg-row justify-content-lg-between">
-    <a class="display-6 header-logo text-dark text-decoration-none" href="{{ route('home') }}">
-        Karrington Creation
-    </a>
-    <div class="header-baseline mt-2">
-    <p class="text-capitalize fs-3 text-capitalize">Un graphisme simplement différent</p>
+<header class="d-flex flex-column o-master-headerp-4 p-4">
+    <div class="col-form-label-lg d-flex flex-column flex-lg-row justify-content-lg-between">
+        <a class="display-6 header-logo text-dark text-decoration-none text-white" href="{{ route('home') }}">Karrington Creation</a>
+        <div class="header-baseline mt-2">
+            <p class="text-capitalize fs-3 text-capitalize">Un graphisme simplement différent</p>
+        </div>
     </div>
-</div>
-    <div id="mobile-btn " class="btn btn-primary d-lg-none mt-3 text-light"> voir le menu</div>
- 
+    <div id="mobile-btn" class="btn btn-primary d-lg-none mt-3 text-light">voir le menu</div>
 </header>
 
 <ul id="main-nav" class="d-flex flex-column flex-lg-row m-0 o-main-nav p-0">
     @if (config('karrot.gallery'))
     <li class="nav-item flex-grow-1 nav-item text-center">
-        <a class=" @if (request()->routeIs('gallery.index')) active @endif d-flex align-items-center justify-content-center p-2" href="{{ route('gallery.index') }}">Galerie photo</a>
+        <a class=" @if (request()->routeIs('gallery.index')) d-lg-none active @endif d-flex align-items-center justify-content-center p-2" href="{{ route('gallery.index') }}">Galerie photo</a>
     </li>
     @endif
 </ul>
