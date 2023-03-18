d7f00808a07daee32be9bc27d1658287398bed76
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-03-18 09:45:38 +0100

    update css

--- resources/views/front/header-nav.blade.php
+++ resources/views/front/header-nav.blade.php
@@ -1,13 +1,12 @@
 <header class="d-flex flex-column o-master-headerp-4 p-4">
     <div class="col-form-label-lg d-flex flex-column flex-lg-row justify-content-lg-between">
-        <a class="display-6 header-logo text-dark text-decoration-none text-white" href="{{ route('home') }}">Karrington Creation</a>
+        <a class="ddisplay-6 text-body text-decoration-none" href="{{ route('home') }}">Karrington Creation</a>
         <div class="header-baseline mt-2">
-            <p class="text-capitalize fs-3 text-capitalize">Un graphisme simplement différent</p>
+            <p class="fs-3 text-body text-capitalize text-nowrap">Un graphisme simplement différent</p>
         </div>
     </div>
     <div id="mobile-btn" class="btn btn-primary d-lg-none mt-3">voir le menu</div>
 </header>
-
 <ul id="main-nav" class="d-flex flex-column flex-lg-row m-0 o-main-nav p-0">
     @if (config('karrot.gallery'))
     <li class="nav-item flex-grow-1 nav-item text-center">
