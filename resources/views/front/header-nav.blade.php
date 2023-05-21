e2e7f4d7138237314387e4a35ac23e1e7cd9d8ba
Alban MABIALA <73714183+karringtoncreation@users.noreply.github.com>
2023-05-21 21:07:33 +0200

    update header page

--- resources/views/front/header-nav.blade.php
+++ resources/views/front/header-nav.blade.php
@@ -1,12 +1,12 @@
 <header class="d-flex flex-column p-4">
-    <div class="d-flex flex-column flex-lg-row justify-content-lg-between">
-        <h1><a class="text-decoration-none" href="{{ route('home') }}">Karrington Creation</a></h1>
+        <h1>
+            <a class="text-decoration-none" href="{{ route('home') }}">Karrington Creation</a>
+        </h1>
         <h2>Un graphisme simplement différent</h2>
         <div id="mobile-btn" class="btn btn-primary d-lg-none mt-3">voir le menu</div>
         <div id="ticker" class="o-ticker">
             <p>This is a ticker</p>
         </div>
-    </div>
 </header>
 
 <ul id="main-nav" class="d-flex flex-column flex-lg-row m-0 o-main-nav p-0">
